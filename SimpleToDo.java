4e7a6920c44e420dd755674ee690af1136accf9b
Rodrigo Mejia <rodri.m.sanchez@gmail.com>
2015-09-25 22:17:09 -0400

    added javadocs, and modified the constructors to take advantage of constructor chaining

--- SimpleToDo.java
+++ SimpleToDo.java
@@ -4,15 +4,33 @@
  * @version 1
  */
 public class SimpleToDo {
-    private String name = "New List";
+    private String name;
     private ArrayList tasks;
 
-    public SimpleToDo(String name) {
-        this.name = name;
-    }
-
+    /*
+     * Constructor for a simple to do list.
+     *
+     * @param name the list's name
+     * @param tasks the list's tasks
+     */
     public SimpleToDo(String name, Task[] tasks) {
         this.name = name;
         this.tasks = tasks;
+    }
+
+    /*
+     * Constructor for a simple to do list.
+     *
+     * @param name the list's name
+     */
+    public SimpleToDo(String name) {
+        this(name, null);
+    }
+
+    /*
+     * Constructor for a simple to do list.
+     */
+    public SimpleToDo() {
+        this("New List", null);
     }
 }
--- Task.java
+++ Task.java
@@ -1,5 +1,6 @@
 /*
  * Class representing a task in a to-do list.
+ *
  * @author Rodrigo Mejia
  * @version 1
  */
@@ -7,27 +8,50 @@ public class Task {
     String title;
     String note;
 
+    /*
+     * Constructor for a task.
+     *
+     * @param title the task's title
+     * @param note the task's note
+     */
     public Task(String title, String note) {
         this.title = title;
         this.note = note;
     }
 
+    /*
+     * Constructor for a task.
+     *
+     * @param title the task's title
+     */
     public Task(String title) {
-        this.title = title;
+        this(title, null);
     }
 
+    /*
+     * @return the task's title
+     */
     public String getTitle() {
         return this.title;
     }
 
+    /*
+     * @return the task's note
+     */
     public String getNote() {
         return this.note;
     }
 
+    /*
+     * @param title the task's title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /*
+     * @param note the task's note
+     */
     public void setNote(String note) {
         this.note = note;
     }
